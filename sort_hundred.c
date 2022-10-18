c504b299e798a2ae5d600dd391e4d4a36a5fce61
Talal Alsaiaari <talsaiaa@lab2r1s1.42abudhabi.ae>
2022-10-18 19:29:45 +0400

    going back to the old sort hunned

--- sort_hundred.c
+++ sort_hundred.c
@@ -5,35 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: talsaiaa <talsaiaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/09 22:40:58 by talsaiaa          #+#    #+#             */
-/*   Updated: 2022/10/18 19:01:02 by talsaiaa         ###   ########.fr       */
+/*   Created: 2022/10/18 19:28:10 by talsaiaa          #+#    #+#             */
+/*   Updated: 2022/10/18 19:28:25 by talsaiaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/push_swap.h"
 
-int	find_index(t_list *a, int *c, int start, int end)
+int	find_index(t_list *a, int *c, int something)
 {
 	int	index;
 
 	index = 0;
 	while (index < a->size)
 	{
-		if (a->top[index] >= c[start] && a->top[index] < c[end])
+		if (a->top[index] <= c[something])
 			return (index);
 		index++;
 	}
 	return (0);
 }
 
-void	get_to_top(t_list *a, int *c, int start, int end)
+void	get_to_top(t_list *a, int *c, int something)
 {
 	int	index;
 
-	index = find_index(a, c, start, end);
-	// if (index == 1)
-	// 	swap_a(a);
-	if (index <= (a->size / 2))
+	index = find_index(a, c, something);
+	if (index == 1)
+		swap_a(a);
+	else if (index <= (a->size / 2))
 	{
 		while (index)
 		{
@@ -41,7 +41,7 @@ void	get_to_top(t_list *a, int *c, int start, int end)
 			index--;
 		}
 	}
-	if (index > (a->size / 2))
+	else if (index > (a->size / 2))
 	{
 		while (index < a->size)
 		{
@@ -49,87 +49,88 @@ void	get_to_top(t_list *a, int *c, int start, int end)
 			index++;
 		}
 	}
+	// if (a->top[0] > b->top[0] && b->top[0] > b->top[1] && b->size > 1)
+	// 	rotate_b(b);
 }
 
-void	pushing_chunk_to_b(t_list *a, t_list *b, int *c, int start, int end)
+// int	checking_a(t_list *a, int *c, int something)
+// {
+// 	int	i;
+	
+// 	i = 0;
+// 	while (i < a->size)
+// 	{
+// 		if (a->top[i] > c[something])
+// 			return (1);
+// 		i++;
+// 	}
+// 	return (0);
+// }
+
+void	pushing_chunk_to_b(t_list *a, t_list *b, int *c, int i)
 {
+	int	something;
+
+	something = i;
 	while (a->size)
 	{
-		get_to_top(a, c, start, end);
-		if (a->top[0] > c[end])
+		get_to_top(a, c, something);
+		if (a->top[0] > c[something])
 			break ;
 		push_b(b, a);
 	}
 }
 
-void	pushing_all_chunks(t_list *a, t_list *b, int *c, int offset, int start, int end, int *ac)
+void	pushing_all_chunks(t_list *a, t_list *b, int *c, int *ac)
 {
+	int	i;
+
+	// if (*ac > 100)
+	// 	i = *ac / 10;
+	// else
+	// 	i = *ac / 5;
+	i = 15;
+	if (i > *ac)
+		i = *ac - 1;
 	while (a->size)
 	{
-		pushing_chunk_to_b(a, b, c, start, end);
-		start -= offset;
-		end += offset;
-		if (end > *ac)
-			end = *ac - 1;
+		pushing_chunk_to_b(a, b, c, i);
+		i += 30;
+		if (i > *ac)
+			i = *ac - 1;
 	}
 }
 
-// void	pushing_chunk_back(t_list *b, t_list *a)
-// {
-// 	int	index;
-
-// 	while (b->size)
-// 	{
-// 		index = find_biggest(b, b->size);
-// 		if (index == 1)
-// 			swap_b(b);
-// 		else if (index <= (b->size / 2))
-// 		{
-// 			while (index)
-// 			{
-// 				rotate_b(b);
-// 				index--;
-// 			}
-// 		}
-// 		else if (index > (b->size / 2))
-// 		{
-// 			while (index < b->size)
-// 			{
-// 				reverse_rotate_b(b);
-// 				index++;
-// 			}
-// 		}
-// 		push_a(a, b);
-// 	}
-// }
-
 int	*sort_hundred(t_list *a, t_list *b, int *c, int *ac)
 {
-	int number_of_chunks;
-	int	mid;
-	int	offset;
-	int	start;
-	int	end;
+	int	index;
 
-	if (*ac <= 10)
-		number_of_chunks = 5;
-	else if (*ac > 10 && *ac <= 150)
-		number_of_chunks = 8;
-	else
-		number_of_chunks = 18;
-	mid = *ac / 2;
-	offset = a->size / number_of_chunks;
-	start = mid - offset;
-	end = mid + offset;
-	// if (i > *ac);
-	// 	i = *ac - 1;
-	while (a->size)
+	pushing_all_chunks(a, b, c, ac);
+	while (b->size)
 	{
-		pushing_all_chunks(a, b, c, offset, start, end, ac);
-		// pushing_chunk_back(b, a);
+		index = find_biggest(b, b->size);
+		if (index == 1)
+			swap_b(b);
+		else if (index <= (b->size / 2))
+		{
+			while (index)
+			{
+				rotate_b(b);
+				index--;
+			}
+		}
+		else if (index > (b->size / 2))
+		{
+			while (index < b->size)
+			{
+				reverse_rotate_b(b);
+				index++;
+			}
+		}
+		push_a(a, b);
 	}
-	// int x = 0;
-	// while (x < a->size)
-	// 	printf("a in sort hunned: %d\n", a->top[x++]);
+	int x = 0;
+	while (x < a->size)
+		printf("a in sort hunned: %d\n", a->top[x++]);
 	return (a->top);
 }
